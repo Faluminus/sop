26de882c01f302c779c05165a650cd63f943d9d2
school <semradfilip@email.cz>
2023-04-04 10:27:44 +0200

    Add project files.

---
+++ projekt1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Security;
+
+namespace projekt1
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Inventory inv = new Inventory();
+
+            while (true)
+            {
+                Console.WriteLine("/Add for adding item");
+                Console.WriteLine("/Rem for removing item");
+                Console.WriteLine("/Use for testing item");
+                string command = Console.ReadLine().ToLower();
+                switch (command)
+                {
+                    case "/add":
+                        int input = ReadIntInput("(0) for weapon , (1) for consumable , (2) for armor");
+                        switch (input)
+                        {
+
+                            case 0:
+                                Console.WriteLine("Enter melee name:");
+                                string name = Console.ReadLine();
+
+                                int damage = ReadIntInput($"Enter {name} damage:");
+                                int durability = ReadIntInput($"Enter {name} durability:");
+                                Weapon weapon = new Weapon(name, damage, durability);
+                                inv.Add(weapon);
+
+                                break;
+                            case 1:
+                                Console.WriteLine("Enter consumable name:");
+                                name = Console.ReadLine();
+                                int heal = ReadIntInput("Enter heal amout:");
+                                int expiration = ReadIntInput($"Enter {name} expiration:");
+                                Consumables consumable = new Consumables(name, expiration, heal);
+                                inv.Add(consumable);
+
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter armor name:");
+                                name = Console.ReadLine();
+                                int protection = ReadIntInput($"Enter {name} protection rate");
+                                durability = ReadIntInput($"Enter {name} durability");
+                                Armor armor = new Armor(name, protection, durability);
+                                inv.Add(armor);
+                                break;
+                        }
+                        break;
+                    case "/rem":
+                        WriteInventory(inv);
+                        Console.WriteLine();
+                           string userinput = Console.ReadLine();
+                            foreach(string x in inv.ShowInventory())
+                            {
+                                 if(x.Equals(userinput))
+                                {
+                                    inv.Remove(x);
+                                }
+                            
+                            }
+                   
+                        break;
+                    case "/use":
+                        WriteInventory(inv);
+                        userinput = Console.ReadLine();
+                        Console.WriteLine();
+                        int chill = inv.Use(userinput);
+                        if (!chill.Equals(0))
+                        {
+                            Console.WriteLine($"You can use {userinput} {chill} times");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Item eaten/destroyed");
+                            Console.WriteLine();
+                        }
+                        break;
+                }
+            }
+        }
+        private static int ReadIntInput(string message)
+        {
+            int value;
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+        private static void WriteInventory(Inventory inv)
+        {
+            
+            Console.WriteLine("Select item you want to interact with");
+            Console.WriteLine("-------------------------------");
+            foreach (string x in inv.ShowInventory())
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
---
+++ projekt1/iteminheritance.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System;
+using System.Security.Policy;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.ComponentModel.Design;
+using System.Security;
+using System.Runtime.Remoting.Messaging;
+using System.Configuration;
+using System.ComponentModel;
+
+internal class Inventory
+{
+    private List<Item> items;
+
+    public Inventory()
+    {
+        items = new List<Item>();
+    }
+
+    public void Add(Item item)
+    {
+        items.Add(item);
+    }
+    public void Remove(string name)
+    {
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            Item item = items[items.Count - 1] ;
+            if (item.Name == name)
+            {
+                items.RemoveAt(i);
+            }
+        }
+    }
+    public int Use(string name)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            
+            if (items[i].Name == name)
+            {
+                WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+                switch (Convert.ToString(items[i].GetType()))
+                {
+                    case "Weapon":
+                        int x = (((Weapon)items[i]).Durability = ((Weapon)items[i]).Durability - 1);
+                        if (x.Equals(0))
+                        {
+                            items.RemoveAt(i);
+                        }
+                        return x;
+                    case "Armor":
+                        x = (((Armor)items[i]).Durability = ((Armor)items[i]).Durability -1);
+                        if(x.Equals(0))
+                        {
+                            items.RemoveAt(i);
+                        }
+                        return x;
+                    case "Consumables":
+                        if (((Consumables)items[i]).Expiration.Equals(0))
+                        {
+                           
+                            wplayer.URL = "C:\\Users\\ja\\source\\repos\\projekt1\\projekt1\\Sounds\\hit.mp3";
+                            wplayer.controls.play();
+                        }
+                            items.RemoveAt(i);
+                        return 0;
+                        
+                }
+            }
+        }
+        return 0;
+    }
+    public string[] ShowInventory()
+    {
+        string[] names = new string[items.Count];
+        int i = 0;
+        foreach(Item item in items)
+        {
+            names[i] = item.Name;
+            i++;
+        }
+        return names;
+    }
+  
+}
+public class Item
+{
+    public string Name;
+
+    public Item(string name)
+    {
+        this.Name = name;
+    }
+}
+public class Weapon : Item
+{
+    public int Damage { get; set; }
+    public int Durability { get; set; }
+    
+
+    public Weapon(string name, int damage, int durability) : base(name)
+    {
+        this.Damage = damage;
+        this.Durability = durability;
+    }
+}
+public class Consumables : Item
+{
+    public int Heal { get; set; }
+    public int Expiration { get; set; }
+
+    public Consumables(string name, int expiration, int heal) : base(name)
+    {
+        this.Heal = heal;
+        this.Expiration = expiration;
+        new Thread(Timer).Start();
+    }
+
+    private void Timer()
+    {
+        for (int i = this.Expiration; i >= 0; i--)
+        {
+            this.Expiration = i;
+            Thread.Sleep(999);
+        }
+    }
+}
+public class Armor : Item
+{
+    public int Protection { get; set; }
+    public int Durability { get; set; }
+
+    public Armor(string name ,int protection, int  durability):base(name)
+    {
+        this.Protection = protection;
+        this.Durability = durability;
+    }
+}
+
